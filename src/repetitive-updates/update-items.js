6c08540c270af167eaeb82513aae88c9da7a8ecb
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 12:36:08 +0300

    logic updates and repetitive self invoking fn to loop a given fn with given ms delay

--- src/repetitive-updates/update-items.js
+++ src/repetitive-updates/update-items.js
@@ -1,31 +1,44 @@
 import { delay } from "../helpers/delay.js";
+import getBGTime from "../helpers/get-time.js";
 import { storefront } from "../helpers/help-fetch.js";
 import Product from "../Models/Product.js";
 
-export default async function updateItems() {
+export async function updateItems() {
+  try {
+    const actualItems = await storefront.fetchShopifyProducts();
+    const localItems = await Product.find().lean();
 
-    try {
-        const actualItems = await storefront.fetchShopifyProducts();
-        const localItems = await Product.find().lean();
+    for (const id of actualItems) {
+      let exist = localItems.find((item) => {
+        const itemId = item._id;
+        return itemId == id;
+      });
 
-        for (const id of actualItems) {
-            let exist = localItems.find(item => {
-                const itemId = item._id;
-                return itemId == id
-            });
+      if (exist) continue;
 
-            if(exist) continue;
-
-            const newProduct = new Product({_id :id});
-            await newProduct.save();
-            await delay(350);
-        }
-
-        return actualItems;
-    } catch (error) {
-        console.warn(error);
-        return undefined;
-        
+      const newProduct = new Product({ _id: id });
+      await newProduct.save();
+      await delay(350);
     }
-    
+
+    return actualItems;
+  } catch (error) {
+    console.warn(error);
+    return undefined;
+  }
+}
+
+export function startBackgroundTask(fn, ms) {
+  (async () => {
+    while (true) {
+      let timeNow = getBGTime();
+      console.log(timeNow + `Function : ${fn.name} has started!`);
+      const success = await fn();
+      await delay(ms);
+      if (success) {
+        timeNow = getBGTime();
+        console.log(timeNow + `Function : ${fn.name} has started!`);
+      }
+    }
+  })();
 }
