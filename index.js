b7c544ee2c15987c97694d8a05e283113e0380f4
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 08:33:31 +0300

    Connect to Atlas

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import router from './src/router.js';
 import dotenv from "dotenv";
+import { connectDB } from './src/helpers/mongoose-helpers.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
