3eb4115d32e594b98daa04cec62ddd0c68a7b4b3
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 12:36:43 +0300

    invoke repetitive fn

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import cors from 'cors';
 import router from './src/router.js';
 import dotenv from "dotenv";
 import { connectDB } from './src/helpers/mongoose-helpers.js';
+import { startBackgroundTask, updateItems } from './src/repetitive-updates/update-items.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 await connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(`/`, router);
+
+startBackgroundTask(updateItems, 86400000);
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
