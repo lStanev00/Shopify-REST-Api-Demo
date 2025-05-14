6e7733f960ff6049306e8bb255342ba95ef7ec82
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 12:07:51 +0300

    remvoe the reference to review since we can populate

--- src/Models/Product.js
+++ src/Models/Product.js
@@ -3,13 +3,6 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
   {
     productId: { type: String, required: true },
-    reviews: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review",
-        required: false,
-      },
-    ],
   },
   { timestamps: true }
 );
