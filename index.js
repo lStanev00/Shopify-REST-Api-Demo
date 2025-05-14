0ab3fc0f2765c24d0e534f1c9a3218e1a8088490
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-14 08:17:08 +0300

    Router setup

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import router from './src/router.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
+app.use(`/`, router);
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
