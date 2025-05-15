42aa3d94f395c2cc02610245f759811c7c67a6fa
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 09:10:01 +0300

    the whole file being prettified with an VSC extension Prettier and new logic for modifing votes

--- src/controllers/reviews-ctrl.js
+++ src/controllers/reviews-ctrl.js
@@ -4,58 +4,72 @@ import { Router } from "express";
 
 const reviewsController = Router();
 
-reviewsController.get('/reviews/:productId', getReviews);
-reviewsController.post('/reviews/:productId', postReview);
+reviewsController.get("/reviews/:productId", getReviews);
+reviewsController.post("/reviews/:productId", postReview);
 reviewsController.patch("/reviews/:id/react", reactToReview);
 
 async function getReviews(req, res) {
-    try {
-        const { productId } = req.params;
-        const reviews = await Review.find({ productId });
-        res.json(reviews);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to fetch reviews' });
-    }
+  try {
+    const { productId } = req.params;
+    const reviews = await Review.find({ productId });
+    res.json(reviews);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch reviews" });
+  }
 }
 
 async function postReview(req, res) {
-    const visitorId = req?.headers["x-visitor-id"];
-    if(!visitorId) return res.status(403).end();
-    try {
-        const { productId } = req.params;
+  const visitorId = req?.headers["x-visitor-id"];
+  if (!visitorId) return res.status(403).end();
+  try {
+    const { productId } = req.params;
 
-        const product = await Product.findOne({productId:productId})
-        const review = new Review({ ...req.body, product: product._id, visitorId: visitorId });
-        await review.save();
+    const product = await Product.findOne({ productId: productId });
+    const review = new Review({
+      ...req.body,
+      product: product._id,
+      visitorId: visitorId,
+    });
+    await review.save();
 
-        res.status(201).json(review.toObject());
-    } catch (error) {
-        console.warn(error)
-        res.status(400).json({ error: 'Failed to submit review' });
-    }
+    res.status(201).json(review.toObject());
+  } catch (error) {
+    console.warn(error);
+    res.status(400).json({ error: "Failed to submit review" });
+  }
 }
 
 async function reactToReview(req, res) {
-    try {
-        const visitorId = req?.headers['x-visitor-id'];
-        if(!visitorId) return res.status(403).end();
+  try {
+    const visitorId = req?.headers["x-visitor-id"];
+    if (!visitorId) return res.status(403).end();
 
-        const { id } = req.params;
-        const { type } = req.body; 
-        const review = await Review.findById(id);
-    
-        if (!review) return res.status(404).json({ error: 'Review not found' });
+    const { id } = req.params;
+    const { type } = req.body;
+    const review = await Review.findById(id);
 
-        
-        if(review?.votes[visitorId]?.type == type) delete review.votes[visitorId]
-        else if (type === 'like') review.votes[visitorId] = {type:"liked"}
-        else if (type === "dislike") review.votes[visitorId] = {type:"dislike"}
-    
-        await review.save();
-        res.json(review);
-    } catch (err) {
-        res.status(400).json({ error: 'Failed to react to review' });
+    if (!review) return res.status(404).json({ error: "Review not found" });
+
+    console.log(type);
+    let trigger = false;
+    if (review?.votes[visitorId]?.type == type) delete review.votes[visitorId];
+    else if (type == "like") {
+      review.votes.set(visitorId, { type: "like" });
+      trigger = true
+    } else if (type == "dislike") {
+      review.votes(visitorId, { type: "dislike" });
+      trigger = true;
     }
+
+    if (trigger) review.markModified("votes");
+
+    
+    console.log(review);
+    await review.save();
+    res.json(review.toObject());
+  } catch (err) {
+    res.status(400).json({ error: "Failed to react to review" });
+  }
 }
 
 export default reviewsController;
