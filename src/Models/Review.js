92ec818c73570f4bcb828c1c6de74c5e98edcb33
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-15 08:45:36 +0300

    visitorId added for integrity

--- src/Models/Review.js
+++ src/Models/Review.js
@@ -2,20 +2,18 @@ import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema(
   {
-    productId: { type: String, required: true },
     email: { type: String, required: true },
     title: { type: String },
     name: { type: String, required: true },
     content: { type: String },
     rating: { type: Number, required: true, min: 1, max: 5 },
     media: [{ type: String }],
-    product: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-    ],
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
     votes: {
       type: Map,
       of: new mongoose.Schema(
@@ -23,10 +21,11 @@ const reviewSchema = new mongoose.Schema(
           type: { type: String, enum: ["like", "dislike"], required: true },
           date: { type: Date, default: Date.now },
         },
-        { _id: false } 
+        { _id: false }
       ),
       default: {},
     },
+    visitorId: { type: String, required: true },
   },
   { timestamps: true }
 );
